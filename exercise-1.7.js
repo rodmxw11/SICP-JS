1eb5b8cb2c5ecaeecb34b718ae3215b3db36b4a3
Rodney Maxwell <rodmxw11@users.noreply.github.com>
2021-07-07 23:06:00 -0400

    Create exercise-1.7.js

---
+++ exercise-1.7.js
@@ -0,0 +1,33 @@
+// source1
+// Type your program in here!
+function is_close_enough(prev_guess, guess, x) {
+    return abs(prev_guess-guess)/guess < 0.0000001;
+}
+
+function sqr(n) {
+    return n*n;
+}
+
+function abs(n) {
+    return n<0 ? -n : n;
+}
+
+function next_guess(guess, x) {
+    return avg(x/guess, guess);
+}
+
+function avg(a, b) {
+    return (a+b)/2;
+}
+
+function sqrt_iter(guess, prev_guess, x) {
+    return is_close_enough(guess, prev_guess, x)
+    ? guess
+    : sqrt_iter(next_guess(guess,x), guess, x);
+}
+
+function sqrt(x) {
+    return sqrt_iter(10.0, 1.0, x);
+}
+
+sqrt(1e20);
